cb39f89150d6ad6787c29d344f7425440e6c2d87
Miro Hrončok <miro@hroncok.cz>
2013-01-08 14:34:07 +0100

    Removed no more usefull script

--- remove-perl-provides.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for DEP in `rpm -q --provides perl | cut -d= -f1`; do
-	sed -i "s/BuildRequires:  $DEP//" perl-*.spec slic3r.spec
-	# TODO remove whitespace
-done
-
-if [ "$1" == "--bumpspec" ]; then
-	rpmdev-bumpspec -c "Removed BRs provided by perl package" perl-*.spec slic3r.spec
-fi
